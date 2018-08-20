f45477843782c1966bd59486e320d9f0b1e805ad
강성훈 <kangpual@naver.com>
2018-08-20 15:47:04 +0900

    Update and rename stop.sh to restart.sh

---
+++ restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SFILE=$1
+
+if [ -f "$SFILE" ] ; then
+    sudo docker-compose -f $SFILE stop
+    sudo docker-compose -f $SFILE up -d
+    sudo docker-compose -f $SFILE logs &> $SFILE.log
+    sudo docker-compose -f $SFILE ps
+else
+    echo "Insert docker-compose file name!"
+fi
